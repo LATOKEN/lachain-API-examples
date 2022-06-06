349ceb4296fed52eadcd002e175f042f0e0a76c5
tbssajal <tariq.salam@latoken.com>
2022-06-06 23:59:26 +0600

    added scripts to test pool-tx-replacement and for some example api

---
+++ python/api-requests.py
@@ -0,0 +1,95 @@
+import web3
+from eth_keys import keys
+import eth_utils
+from eth_utils import decode_hex
+from jsonrpcclient import request
+import requests
+import time
+import ethereum
+import random
+
+# Local
+LOCALNET_NODE = "http://localhost:7070"
+# LOCALNET_NODE = "http://localhost:7071"
+# LOCALNET_NODE = "http://localhost:7072"
+# LOCALNET_NODE = "http://localhost:7073"
+# Local
+
+
+NODE = web3.Web3(web3.Web3.HTTPProvider(LOCALNET_NODE))
+SESSION = requests.Session()
+def send_api_request_to_address(address, params , method):
+    payload= {"jsonrpc":"2.0",
+           "method":method,
+           "params":params,
+           "id":0}
+    
+    headers = {'Content-type': 'application/json'}
+    response = SESSION.post(address, json=payload, headers=headers)
+    try:
+        res = response.json()['result']
+        return res
+    except Exception as eer:
+        print(response.json())
+        print("exception: " + format(eer))
+        return eer
+
+def send_api_request(params , method):
+    return send_api_request_to_address(LOCALNET_NODE, params, method)
+
+def block_by_number(block, full_tx):
+    return send_api_request([ block, full_tx ] , "eth_getBlockByNumber")
+
+def block_by_hash(block, full_tx):
+    return send_api_request([ block, full_tx ] , "eth_getBlockByHash")
+
+def block_number():
+    return send_api_request([] , "eth_blockNumber")
+
+def tx_receipt(tx_hash):
+    return send_api_request([ tx_hash ] , "eth_getTransactionReceipt")
+
+def tx_by_hash(tx_hash):
+    return send_api_request([ tx_hash ] , "eth_getTransactionByHash")
+
+def fe_get_balance(address):
+    return send_api_request([ address ] , "fe_getBalance")
+
+def get_validators():
+    return send_api_request([] , "bcn_validators")
+
+def test(block):
+    return send_api_request([ block ] , "testing")
+
+def get_validators():
+    return send_api_request([] , "bcn_validators")
+
+def la_validator_info(pubkey):
+    return send_api_request([ pubkey ] , "la_validator_info")
+
+def get_balance(address):
+    return send_api_request([address , "latest"] , "eth_getBalance")
+
+def update_nonce(address):
+    method = "eth_getTransactionCount"
+    params = [
+        address,
+        "latest"
+    ]
+    nonce = send_api_request(params , method)
+    int_nonce = nonce
+    params = [
+        address,
+        "pending"
+    ]
+    nonce = send_api_request(params , method)
+    int_nonce = max(int_nonce , nonce)
+    return int_nonce
+
+def get_chain_id():
+    return int(send_api_request([], "eth_chainId"), 16)
+
+
+if __name__ == "__main__":
+    while(True):
+        print(int(block_number(),16))
---
+++ python/pool-tx-replacement.py
@@ -0,0 +1,196 @@
+import web3
+from eth_keys import keys
+import eth_utils
+from eth_utils import decode_hex
+from jsonrpcclient import request
+import requests
+import time
+import ethereum
+import random
+
+# Local
+#LOCALNET_NODE = "http://localhost:7071"
+LOCALNET_NODE = "http://localhost:7070"
+#LOCALNET_NODE = "http://localhost:7072"
+#LOCALNET_NODE = "http://localhost:7073"
+# Local
+
+NODE = web3.Web3(web3.Web3.HTTPProvider(LOCALNET_NODE))
+SESSION = requests.Session()
+def send_api_request_to_address(address, params , method):
+    payload= {"jsonrpc":"2.0",
+           "method":method,
+           "params":params,
+           "id":0}
+    
+    headers = {'Content-type': 'application/json'}
+    response = SESSION.post(address, json=payload, headers=headers)
+    try:
+        res = response.json()['result']
+        return res
+    except Exception as eer:
+        print(response.json())
+        print("exception: " + format(eer))
+        return eer
+
+def send_api_request(params , method):
+    return send_api_request_to_address(LOCALNET_NODE, params, method)
+
+def send_raw_tx(raw_tx):
+    return send_api_request([raw_tx], "eth_sendRawTransaction")
+
+def tx_by_hash(tx_hash):
+    return send_api_request([ tx_hash ] , "eth_getTransactionByHash")
+
+def update_nonce(address):
+    method = "eth_getTransactionCount"
+    params = [
+        address,
+        "latest"
+    ]
+    nonce = send_api_request(params , method)
+    int_nonce = nonce
+    params = [
+        address,
+        "pending"
+    ]
+    nonce = send_api_request(params , method)
+    int_nonce = max(int_nonce , nonce)
+    return int_nonce
+
+def get_address_from_private_key(private_key_byte):
+    return ethereum.utils.checksum_encode(ethereum.utils.privtoaddr(private_key_byte))
+
+def generate_random_key(byte_len):
+    hex_len = byte_len * 2
+    key = "0x"
+    for _ in range(hex_len):
+        digit = hex(random.randint(0,15))
+        key = key + digit[2]
+    return key
+
+def get_chain_id():
+    return int(send_api_request([], "eth_chainId"), 16)
+
+def transaction_builder(from_address, to_address, amount, gas_price, nonce):
+    transaction = {
+        "from": from_address,
+        "to": to_address,
+        "value": amount,
+        "gas": 100000000,
+        "gasPrice": gas_price,
+        "nonce": nonce,
+        "chainId": CHAIN_ID
+    }
+    return transaction
+
+def generate_random_private_key():
+    return generate_random_key(32)
+
+def generate_random_address():
+    private_key = generate_random_private_key()
+    return get_address_from_private_key(decode_hex(private_key))
+
+def give_me_some_money(my_address, amount):
+    private_key_bytes = decode_hex('0xd95d6db65f3e2223703c5d8e205d98e3e6b470f067b0f94f6c6bf73d4301ce48')
+    address = ethereum.utils.checksum_encode(ethereum.utils.privtoaddr(private_key_bytes))
+    tx = transaction_builder(address, my_address, amount, 1, update_nonce(address))
+    signed_tx = web3.eth.Account.signTransaction(tx, private_key_bytes)
+    raw_tx = web3.Web3.toHex(signed_tx.rawTransaction)
+    tx_hash = send_raw_tx(raw_tx)
+    print("requested for money transfer, tx hash: " + tx_hash)
+    tx_receipt = NODE.eth.wait_for_transaction_receipt(tx_hash)
+    print("Money transfer complete. Tx receipt:")
+    print(tx_receipt)
+
+saved_txes = []
+def send_txes(private_key_hex, tx_count, gas_price, nonce, save_txes = False):
+    private_key = decode_hex(private_key_hex)
+    address = get_address_from_private_key(private_key)
+    del saved_txes[:]
+    for _ in range(tx_count):
+        tx = transaction_builder(address, generate_random_address(), 0, gas_price, nonce)
+        signed_tx = web3.eth.Account.signTransaction(tx, private_key)
+        raw_tx = web3.Web3.toHex(signed_tx.rawTransaction)
+        tx_hash = send_api_request([raw_tx] , "eth_sendRawTransaction")
+        actual_tx_hash = web3.Web3.toHex(signed_tx.hash)
+
+        if actual_tx_hash == tx_hash:
+            if save_txes == True:
+                saved_txes.append(tx_hash)
+            print("transaction added to pool, tx hash: " + tx_hash)
+            nonce = nonce + 1
+        else:
+            print("could not add to pool, result: " + format(tx_hash))
+
+    print("Printing saved txes, total: " + format(len(saved_txes)))
+    for tx in saved_txes:
+        print(tx)
+
+    return nonce
+
+
+CHAIN_ID = get_chain_id()
+
+if __name__ == "__main__":
+    print(CHAIN_ID)
+    random_private_keys = []
+    nonces = []
+    total_key = 10
+    for _ in range(total_key):
+        random_private_keys.append(generate_random_private_key())
+        nonces.append(0)
+    testing_key = generate_random_private_key()
+    my_nonce = 0
+    
+    for private_key in random_private_keys:
+        address = get_address_from_private_key(decode_hex(private_key))
+        give_me_some_money(address, 2*pow(10, 9))
+    
+    testing_address = get_address_from_private_key(decode_hex(testing_key))
+    give_me_some_money(testing_address, 3*pow(10, 9))
+    
+    tx_count = 10
+    for iter in range(total_key - 1):
+        successful_txes = send_txes(random_private_keys[iter], tx_count, 3, nonces[iter])
+        print("successfully sent: " + format(successful_txes))
+        print("failed: " + format(tx_count - successful_txes))
+    successful_txes = send_txes(testing_key, tx_count, 2, my_nonce, True)
+    print("successfully sent: " + format(successful_txes))
+    print("failed: " + format(tx_count - successful_txes))
+    if len(saved_txes) != successful_txes:
+        print("txes are not saved properly, got " + format(len(saved_txes)) + " txes saved")
+    old_txes = []
+    for tx in saved_txes:
+        old_txes.append(tx)
+
+    # trying to replace txes, but it should fail as gas price is equal or lower, gas price must be higher
+    successful_txes = send_txes(testing_key, tx_count, 2, 0)
+    print("successfully sent: " + format(successful_txes))
+    print("failed: " + format(tx_count - successful_txes))
+    
+    successful_txes = send_txes(testing_key, tx_count, 1, 0)
+    print("successfully sent: " + format(successful_txes))
+    print("failed: " + format(tx_count - successful_txes))
+    
+    # this should replace pool txes
+    successful_txes = send_txes(testing_key, tx_count, 3, 0, True)
+    print("successfully sent: " + format(successful_txes))
+    print("failed: " + format(tx_count - successful_txes))
+    
+    if len(saved_txes) != successful_txes:
+        print("txes are not saved properly, got " + format(len(saved_txes)) + " txes saved")
+    new_txes = []
+    for tx in saved_txes:
+        new_txes.append(tx)
+    if len(new_txes) != len(old_txes):
+        print("old tx count: " + format(len(old_txes)) + ", new tx count: " + format(len(new_txes)))
+
+    print("printing old txes")
+    for tx in old_txes:
+        print(tx + " " + format(tx_by_hash(tx)))
+
+    print("printing new txes")
+    for tx in new_txes:
+        print(tx + " " + format(tx_by_hash(tx)))
+    
