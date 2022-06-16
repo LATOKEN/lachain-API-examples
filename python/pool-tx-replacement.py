9e94b1f3896d9841886a70b1724cd74d26a31ea2
tbssajal <tariq.salam@latoken.com>
2022-06-16 18:26:54 +0600

    updated script for new updated functionality of tx replacement

--- python/pool-tx-replacement.py
+++ python/pool-tx-replacement.py
@@ -117,7 +117,7 @@ def send_txes(private_key_hex, tx_count, gas_price, nonce, save_txes = False):
 
         if actual_tx_hash == tx_hash:
             if save_txes == True:
-                saved_txes.append(tx_hash)
+                saved_txes.append((tx_hash, tx))
             print("transaction added to pool, tx hash: " + tx_hash)
             nonce = nonce + 1
         else:
@@ -128,6 +128,35 @@ def send_txes(private_key_hex, tx_count, gas_price, nonce, save_txes = False):
         print(tx)
 
     return nonce
+
+def send_given_txes(private_key_hex, tx_list, gas_price, save_txes = False):
+    private_key = decode_hex(private_key_hex)
+    del saved_txes[:]
+    count = 0
+    print("trying to replace old txes")
+    for item in tx_list:
+        tx = item[1]
+        print("old tx: " + format(tx))
+        tx["gasPrice"] = gas_price
+        print("new tx: " + format(tx))
+        signed_tx = web3.eth.Account.signTransaction(tx, private_key)
+        raw_tx = web3.Web3.toHex(signed_tx.rawTransaction)
+        tx_hash = send_api_request([raw_tx] , "eth_sendRawTransaction")
+        actual_tx_hash = web3.Web3.toHex(signed_tx.hash)
+
+        if actual_tx_hash == tx_hash:
+            if save_txes == True:
+                saved_txes.append((tx_hash,tx))
+            print("transaction added to pool, tx hash: " + tx_hash)
+            count = count + 1
+        else:
+            print("could not add to pool, result: " + format(tx_hash))
+            
+    print("Printing saved txes, total: " + format(len(saved_txes)))
+    for tx in saved_txes:
+        print(tx)
+    
+    return count
 
 
 CHAIN_ID = get_chain_id()
@@ -161,6 +190,8 @@ if __name__ == "__main__":
         successful_txes = send_txes(random_private_keys[iter], tx_count, high_gas_price, nonces[iter])
         print("successfully sent: " + format(successful_txes))
         print("failed: " + format(tx_count - successful_txes))
+
+    #sending low_gas_price tx
     successful_txes = send_txes(testing_key, tx_count, low_gas_price, my_nonce, True)
     print("successfully sent: " + format(successful_txes))
     print("failed: " + format(tx_count - successful_txes))
@@ -179,8 +210,13 @@ if __name__ == "__main__":
     print("successfully sent: " + format(successful_txes))
     print("failed: " + format(tx_count - successful_txes))
     
-    # this should replace pool txes
-    successful_txes = send_txes(testing_key, tx_count, high_gas_price, 0, True)
+    # trying to replace txes, but it should fail other fields do not match
+    successful_txes = send_txes(testing_key, tx_count, high_gas_price, 0)
+    print("successfully sent: " + format(successful_txes))
+    print("failed: " + format(tx_count - successful_txes))
+
+    # this identical txes should replace the old ones because of high gas price
+    successful_txes = send_given_txes(testing_key, old_txes, high_gas_price, True)
     print("successfully sent: " + format(successful_txes))
     print("failed: " + format(tx_count - successful_txes))
     
@@ -194,9 +230,31 @@ if __name__ == "__main__":
 
     print("printing old txes")
     for tx in old_txes:
-        print(tx + " " + format(tx_by_hash(tx)))
+        print("hash: " + tx[0])
+        print("full tx " + format(tx[1]))
+        print("tx by hash " + format(tx_by_hash(tx[0])))
 
     print("printing new txes")
     for tx in new_txes:
-        print(tx + " " + format(tx_by_hash(tx)))
+        print("hash: " + tx[0])
+        print("full tx " + format(tx[1]))
+        print("tx by hash " + format(tx_by_hash(tx[0])))
+
+    time.sleep(6)
+    # this identical txes should not replace the old ones
+    successful_txes = send_given_txes(testing_key, old_txes, high_gas_price + 1, True)
+    print("successfully sent: " + format(successful_txes))
+    print("failed: " + format(tx_count - successful_txes))
+
+    if len(saved_txes) != successful_txes:
+        print("txes are not saved properly, got " + format(len(saved_txes)) + " txes saved")
+    new_abnormal_txes = []
+    for tx in saved_txes:
+        new_abnormal_txes.append(tx)
+    print("printing abnormal txes")
+    for tx in new_abnormal_txes:
+        print("hash: " + tx[0])
+        print("full tx " + format(tx[1]))
+        print("tx by hash " + format(tx_by_hash(tx[0])))
+
     
