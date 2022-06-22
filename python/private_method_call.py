8b5b7f1dba74690662de2aa4c579603ad7ccbc0b
kunal.goyal <kunal.goyal@latoken.com>
2022-06-22 18:45:50 +0530

    added headers

--- python/private_method_call.py
+++ python/private_method_call.py
@@ -1,3 +1,5 @@
+from time import time
+from sqlalchemy import null
 import web3
 from eth_utils import decode_hex
 import requests
@@ -11,9 +13,9 @@ def transaction_builder():
                 "method": "eth_sendTransaction" ,
                 "params": 
                     [{
-                        "from" : "0x87b74be043eba0ae3f0fef0758e8cd1388cf928a",
+                        "from" : "0x73ba7dd59432eb32cb99db02e339f679eacb4514",
                         "to": "0xDA9B931dAA5c211004240C0aCEFE8363176E99B4",
-                        "value": "0x9184e72a",
+                        "value": "0x174876",
                         "data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675",
                         "gas":"0x2e1a62",
                         "gasPrice": "0x9184e72a000"
@@ -23,11 +25,19 @@ def transaction_builder():
     return transaction
 
 tx = transaction_builder()
+
 # private_key_bytes = decode_hex('0xd95d6db65f3e2223703c5d8e205d98e3e6b470f067b0f94f6c6bf73d4301ce48')
 # signed_tx = web3.eth.Account.signTransaction(tx, private_key_bytes)
 
 # print(signed_tx)
 
-response = requests.post(url, json=tx)
+signature = "test"
+timestamp = "dfls"
+
+headers = {'Content-type': 'application/json',
+           'Signature': signature,
+           'Timestamp': timestamp}
+
+response = requests.post(url, json=tx, headers = headers)
 
 print(response.json())
