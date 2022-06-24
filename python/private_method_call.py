d0f34a60fd6fd0f0c77640ec152ad3ed14d51251
kunalgoyal9 <kunalgoyal.goyal9@gmail.com>
2022-06-24 21:51:09 +0530

    signature according to lachain

--- python/private_method_call.py
+++ python/private_method_call.py
@@ -1,3 +1,4 @@
+from inspect import signature
 from time import time
 from numpy import sign
 #from sqlalchemy import null
@@ -15,29 +16,20 @@ def transaction_builder():
     transaction = {
                 "jsonrpc": "2.0" ,
                 "method": "eth_sendTransaction" ,
-                "params": 
-                    [{
-                        "from" : "0x73ba7dd59432eb32cb99db02e339f679eacb4514",
-                        "to": "0xDA9B931dAA5c211004240C0aCEFE8363176E99B4",
-                        "value": "0x174876",
-                        "data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675",
-                        "gas":"0x2e1a62",
-                        "gasPrice": "0x9184e72a000"
-                    } ],
+                "params":{
+                            "from" : "0x84c5ede5846efd970ea777c1122ef130b4af2bdd",
+                            "to": "0xDA9B931dAA5c211004240C0aCEFE8363176E99B4",
+                            "value": "0x17487",
+                            "data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675",
+                            "gas":"0x2e1a62",
+                            "gasPrice": "0x9184e72a000"
+                        },
                  "id": 1,
             };
     return transaction
 
-def get_random_hex_message(len):
-    len = len * 2
-    msg = "0x"
-    for x in range(len):
-        digit = hex(random.randint(0,15))
-        msg = msg + digit[2]
-    return msg
-
 def generate_private_key():
-    private_key_str = "0xa427dff0b296ab52adce3ba144f6646096dce9b24a035e008b25ac04e516c32a"
+    private_key_str = "0xddd2c0107d2c999dd65b1e3cced4263a03236e92dcb8174dfcc5af30d98692cb"
     key_bytes = decode_hex(private_key_str)
     return keys.PrivateKey(key_bytes)
 
@@ -48,11 +40,31 @@ def sign_random_message(msg):
     signed = web3.eth.Account.sign_message(signable , private_key.to_hex())
     return signed.signature.hex()
 
-tx = transaction_builder()
-tx_str = json.dumps(tx)
+def sign_message(tx, timestamp):
+    serializedParams = ""
+    
+    for tx_param in tx['params']:
+        serializedParams += tx_param + tx['params'][tx_param]
 
-signature = sign_random_message(tx_str)
+    serializedParams = tx['method'] + serializedParams + timestamp
+    
+    print("serialized params: ", serializedParams)
+    
+    signable = encode_defunct(text = serializedParams)
+    
+    private_key = generate_private_key()
+    signed = web3.eth.Account.sign_message(signable , private_key.to_hex())
+    
+    print("signed: ", signed)
+    
+    return signed.signature.hex()
+    
+tx = transaction_builder()
+
+# tx_str = json.dumps(tx)
+# signature = sign_random_message(tx_str)
 timestamp = str(int(time()))
+signature = sign_message(tx, timestamp)
 
 print(signature)
 print(timestamp)
